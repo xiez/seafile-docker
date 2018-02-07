0a7f6ce253195ef08bdc49c0db22d908f61ee94e
zhengxie <xiez1989@gmail.com>
2018-02-07 14:50:27 +0800

    [dev] Add docker compose files

--- dev/setenv.sh
+++
@@ -1,4 +0,0 @@
-export CCNET_CONF_DIR=/code/seafile-server/tests/conf
-export SEAFILE_CONF_DIR=/code/seafile-server/tests/conf/seafile-data
-export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:/code/seahub/thirdpart:$PYTHONPATH
-export PYTHONPATH=/code:$PYTHONPATH     # evetns, search, etc...
--- dev/start-services.sh
+++ dev/start-services.sh
@@ -11,15 +11,30 @@ echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 export HOME=/home/user
 
+if [ "$IS_PRO_VERSION" ]; then
+    echo "Starting PRO services......"
 
-# start services
-/usr/local/bin/gosu user ccnet-server -c /code/seafile-server/tests/conf -f - & 
-sleep 2
+    echo '/code/seafile-pro-server/tests/conf' > /code/seafile-pro-server/tests/conf/seafile.ini
 
-/usr/local/bin/gosu user seaf-server -c /code/seafile-server/tests/conf -d /code/seafile-server/tests/conf/seafile-data -f -l - &
-sleep 2
+    # start services
+    /usr/local/bin/gosu user ccnet-server -c /code/seafile-pro-server/tests/conf -L $LICENSE_DIR -f - &
+    sleep 2
 
-. /code/setenv.sh
+    /usr/local/bin/gosu user seaf-server -c /code/seafile-pro-server/tests/conf -d /code/seafile-pro-server/tests/conf/seafile-data -f -l - &
+    sleep 2
+
+    /usr/local/bin/gosu user python -m seafevents.main --loglevel=debug --logfile=/tmp/events.log --config-file=/code/seafevents/events.conf --reconnect &
+    # sleep 2
+else
+    echo "Starting CE services......"
+
+    # start services
+    /usr/local/bin/gosu user ccnet-server -c /code/seafile-server/tests/conf -f - &
+    sleep 2
+
+    /usr/local/bin/gosu user seaf-server -c /code/seafile-server/tests/conf -d /code/seafile-server/tests/conf/seafile-data -f -l - &
+    sleep 2
+fi
+
 cd /code/seahub
-python manage.py migrate --noinput && python manage.py runserver 0.0.0.0:8000
-
+/usr/local/bin/gosu user python manage.py migrate --noinput && /usr/local/bin/gosu user python manage.py runserver 0.0.0.0:8000
